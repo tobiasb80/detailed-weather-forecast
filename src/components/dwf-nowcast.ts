884f4a86d99dc1ba6626f557135dbcee96a429b9
Tobias Bischoff <tobischoff@gmx.de>
2026-03-29 15:05:36 +0000

    fix: nowcast

--- src/components/dwf-nowcast.ts
+++ src/components/dwf-nowcast.ts
@@ -49,8 +49,8 @@ export class DWFNowcast extends LitElement {
     const labelOffset = this._computeLabelOffset(this._containerWidth || this.clientWidth);
     const labels =
       this._barStride > 1
-        ? [localize('nowcast.now'), '20m', '40m', '60m']
-        : [localize('nowcast.now'), '10m', '20m', '30m', '40m', '50m', '60m'];
+        ? [localize('card.nowcast.now'), '20m', '40m', '60m']
+        : [localize('card.nowcast.now'), '10m', '20m', '30m', '40m', '50m', '60m'];
 
     return html`
       <div class=${classMap({ 'nowcast-bars': true })} style=${styleMap({ '--dwf-nowcast-gap': `${this._barGap}px` })}>
